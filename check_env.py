86ecc5992ef50a49b4dc957cb588ce0ecf64cfdd
mitsuo0114 <oono0114@gmail.com>
2018-12-06 16:39:33 +0900

    initial commit

---
+++ check_env.py
@@ -0,0 +1,25 @@
+import numpy as np
+print(f"numpy = {np.__version__}")
+
+import pandas as pd
+print(f"pandas = {pd.__version__}")
+
+import sklearn as sk
+print(f"sklearn = {sk.__version__}")
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+print(f"matplotlib = {mpl.__version__}")
+
+import seaborn as sns
+print(f"seaborn = {sns.__version__}")
+
+import pydotplus
+import graphviz
+print(f"graphviz = {graphviz.__version__}")
+
+# import tensorflow as tf
+# tf.__version__
+
+# import keras
+# keras.__version__
