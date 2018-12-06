47516ef9fc3191657c3d6ae5784baeb274ec6386
mitsuo0114 <oono0114@gmail.com>
2018-12-06 17:40:53 +0900

    add graph

---
+++ 1/1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+x = np.arange(100)
+np.random.seed(seed=1234)
+y = np.random.normal(loc=x, scale=10)
+
+y_true = x.copy()
+
+plt.scatter(x, y, marker='o')
+plt.plot(x, y_true, linestyle='--', linewidth=3, color='orange', label='True model')
+plt.grid(which='major',color='black',linestyle=':')
+plt.grid(which='minor',color='black',linestyle=':')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.legend(loc='best')
+plt.show()
